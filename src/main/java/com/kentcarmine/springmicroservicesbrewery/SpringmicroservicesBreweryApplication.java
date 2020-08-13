9a05001188d2c12391a31dc505b5140ce8af1450
Kent Carmine <5168853+KentCarmine@users.noreply.github.com>
2020-08-13 17:53:46 -0500

    Initial commit

---
+++ src/main/java/com/kentcarmine/springmicroservicesbrewery/SpringmicroservicesBreweryApplication.java
@@ -0,0 +1,13 @@
+package com.kentcarmine.springmicroservicesbrewery;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringmicroservicesBreweryApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SpringmicroservicesBreweryApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/kentcarmine/springmicroservicesbrewery/SpringmicroservicesBreweryApplicationTests.java
@@ -0,0 +1,13 @@
+package com.kentcarmine.springmicroservicesbrewery;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringmicroservicesBreweryApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
