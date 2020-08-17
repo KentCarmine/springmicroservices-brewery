4060743d27ee4f5695a95741f273df9ba6ba9667
Kent Carmine <5168853+KentCarmine@users.noreply.github.com>
2020-08-17 14:51:58 -0500

    Added POST, PUT, and DELETE routes to CustomerController

--- src/main/java/com/kentcarmine/springmicroservicesbrewery/web/controller/CustomerController.java
+++ src/main/java/com/kentcarmine/springmicroservicesbrewery/web/controller/CustomerController.java
@@ -3,12 +3,10 @@ package com.kentcarmine.springmicroservicesbrewery.web.controller;
 import com.kentcarmine.springmicroservicesbrewery.services.CustomerService;
 import com.kentcarmine.springmicroservicesbrewery.web.model.CustomerDto;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpHeaders;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PathVariable;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
 import java.util.UUID;
 
@@ -26,5 +24,26 @@ public class CustomerController {
     @GetMapping("/{customerId}")
     public ResponseEntity<CustomerDto> getCustomerById(@PathVariable("customerId") UUID customerId) {
         return new ResponseEntity<>(customerService.getCustomerById(customerId), HttpStatus.OK);
+    }
+
+    @PostMapping
+    public ResponseEntity createNewCustomer(@RequestBody CustomerDto customerDto) {
+        CustomerDto savedCustomer = customerService.saveNewCustomer(customerDto);
+
+        HttpHeaders headers = new HttpHeaders();
+        headers.add("Location", "/api/v1/customer" + savedCustomer.getId().toString());
+
+        return new ResponseEntity(headers, HttpStatus.CREATED);
+    }
+
+    @PutMapping("/{customerId}")
+    @ResponseStatus(HttpStatus.NO_CONTENT)
+    public void updateCustomer(@PathVariable("customerId") UUID customerId, @RequestBody CustomerDto customerDto) {
+        customerService.updateCustomer(customerId, customerDto);
+    }
+
+    @DeleteMapping("/{customerId}")
+    public void deleteCustomer(@PathVariable("customerId") UUID customerId) {
+        customerService.deleteById(customerId);
     }
 }
