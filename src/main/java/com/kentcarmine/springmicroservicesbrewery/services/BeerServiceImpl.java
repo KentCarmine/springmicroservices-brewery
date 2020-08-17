d1703d9195c79c9b6eb1d068f8cd04069fc0ccc6
Kent Carmine <5168853+KentCarmine@users.noreply.github.com>
2020-08-17 14:35:04 -0500

    Added POST, PUT, and DELETE routes to BeerController

--- src/main/java/com/kentcarmine/springmicroservicesbrewery/services/BeerServiceImpl.java
+++ src/main/java/com/kentcarmine/springmicroservicesbrewery/services/BeerServiceImpl.java
@@ -1,10 +1,12 @@
 package com.kentcarmine.springmicroservicesbrewery.services;
 
 import com.kentcarmine.springmicroservicesbrewery.web.model.BeerDto;
+import lombok.extern.slf4j.Slf4j;
 import org.springframework.stereotype.Service;
 
 import java.util.UUID;
 
+@Slf4j
 @Service
 public class BeerServiceImpl implements BeerService {
     @Override
@@ -14,5 +16,23 @@ public class BeerServiceImpl implements BeerService {
                 .beerName("Galaxy Cat")
                 .beerStyle("Pale Ale")
                 .build();
+    }
+
+    @Override
+    public BeerDto saveNewBeer(BeerDto beerDto) {
+        return BeerDto.builder()
+                .id(UUID.randomUUID())
+                .build();
+    }
+
+    @Override
+    public void updateBeer(UUID beerId, BeerDto beerDto) {
+        // TODO Impl - would add real impl to update beer
+        log.debug("Updating beer with ID "+ beerId);
+    }
+
+    @Override
+    public void deleteById(UUID beerId) {
+        log.debug("Deleting a beer...");
     }
 }
--- src/main/java/com/kentcarmine/springmicroservicesbrewery/web/controller/BeerController.java
+++ src/main/java/com/kentcarmine/springmicroservicesbrewery/web/controller/BeerController.java
@@ -3,12 +3,10 @@ package com.kentcarmine.springmicroservicesbrewery.web.controller;
 import com.kentcarmine.springmicroservicesbrewery.services.BeerService;
 import com.kentcarmine.springmicroservicesbrewery.web.model.BeerDto;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpHeaders;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PathVariable;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
 import java.util.UUID;
 
@@ -26,6 +24,29 @@ public class BeerController {
     @GetMapping("/{beerId}")
     public ResponseEntity<BeerDto> getBeer(@PathVariable("beerId") UUID beerId) {
         return new ResponseEntity<>(beerService.getBeerById(beerId), HttpStatus.OK);
+    }
+
+    @PostMapping
+    public ResponseEntity handlePost(@RequestBody BeerDto beerDto) {
+        BeerDto savedDto = beerService.saveNewBeer(beerDto);
+
+        HttpHeaders headers = new HttpHeaders();
+        headers.add("Location", "/api/v1/beer" + savedDto.getId().toString()); // TODO: Add hostname to url
+
+        return new ResponseEntity(headers, HttpStatus.CREATED);
+    }
+
+    @PutMapping("/{beerId}")
+    public ResponseEntity handleUpdate(@PathVariable("beerId") UUID beerId, @RequestBody BeerDto beerDto) {
+        beerService.updateBeer(beerId, beerDto);
+
+        return new ResponseEntity(HttpStatus.NO_CONTENT);
+    }
+
+    @DeleteMapping("/{beerId}")
+    @ResponseStatus(HttpStatus.NO_CONTENT)
+    public void deleteBeer(@PathVariable("beerId") UUID beerId) {
+        beerService.deleteById(beerId);
     }
 
 }
